1967e32b9c91523c896ce8039d929cf3dbe9bd94
camwhite <whiteceric@gmail.com>
2022-01-10 16:59:58 -0700

    Create React App and electron scripts

---
+++ public/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron')
+
+require('@electron/remote/main').initialize()
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      enableRemoteModule: true,
+    },
+  })
+  win.maximize()
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
